ddc8bae23a0bfeec21a462c37d1d71f6ff255c8d
yoonshinaelim <ysal4505@gamil.com>
2025-03-07 09:36:48 +0900

    옮김

---
+++ js/common.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+});
+
+// main list
+
+ScrollReveal().reveal('#container .column1 .btn', {interval:200});
+ScrollReveal().reveal('#container .column2 .btn', {interval:200});
+ScrollReveal().reveal('#container .column3 .btn', {interval:200});
