00632539faabc11df79552b53e5f02b75b34e467
yoonshinaelim <ysal4505@gamil.com>
2025-03-07 15:03:13 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -1,21 +1,17 @@
 const body = document.querySelector("#body"),
-      menuButton = document.querySelector("#menu"),
-      menuList = document.querySelector("#menuList"),
-      header = document.querySelector("#header");
+menuButton = document.querySelector("#menu"),
+menuList = document.querySelector("#menuList"),
+header = document.querySelector("#header");
 
 menuButton.addEventListener("click", function(){
-    var toggle = this.classList.toggle("on");
-    toggle;
+    this.classList.toggle("on");
     header.classList.toggle("on");
     menuList.classList.toggle("on");
     body.classList.toggle("hidden");
-
 });
-
 //main list
 function scrollRealStart(){
-    var mainList = "#mainList li";
-    ScrollReveal().reveal(mainList, {reset:false, delay :10, distance: '50px', opacity:.5});
+    ScrollReveal().reveal("#mainList li", {reset:false, delay :10, distance: '50px', opacity:.5});
 }
 scrollRealStart();
 
